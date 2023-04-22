62f5b2ea1d78798c044eb3f92a23c36ec090123d
Ghost Host <gaby18vsilva@gmail.com>
2023-04-22 19:36:51 -0400

    update routes - add users

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
     res.render('index', { title : 'Página Inicial' })
 });
 
+router.get('/add', (req, res) => {
+    res.render('add_users', { title: 'Add Users' });
+});
+
 module.exports = router;
