eb5d4fbe4b9ef04a40ad1366437d408857dd1671
Ghost Host <gaby18vsilva@gmail.com>
2023-04-24 22:00:14 -0400

    adiciona novo user e redireciona pra homepage

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users');
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname+"_"+Date.now()+"_"+file.originalname);
+    },
+});
+
+var upload = multer({
+    storage: storage,
+}).single('imagem');
+
+router.post('/add', upload, (req, res) => {
+    const user = new User({
+        nome: req.body.nome,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        imagem: req.file.filename,
+    });
+    user.save((err) => {
+        if(err){
+            res.json({message: err.message, type: 'danger'});
+        }else{
+            req.session.message = {
+                type: 'success',
+                message: 'Usuário adicionado com sucesso!'
+            };
+            res.redirect("/");
+        }
+    })
+});
 
 router.get('/', (req, res) => {
-    res.render('index', { title : 'Página Inicial' })
+    User.find().exec((err, users) => {
+        if(err){
+            res.json({ message: err.message });
+        }else{
+            res.render('index', {
+                title: 'Página Inicial',
+                users: users,
+            })
+        }
+    })
 });
 
 router.get('/add', (req, res) => {
